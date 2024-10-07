b41d19e15ae8536c20cd68efa4bbbdb202d8939f
Ivan Yordanov <55908650+ivo0509@users.noreply.github.com>
2024-10-07 00:28:19 -0900

    Added last exercise from Data types

---
+++ Programming Fundamentals-and-Unit testing-September-2024/Data Types, Variables and Key Concepts/06.Aquarium/Program.cs
@@ -0,0 +1,13 @@
+﻿// Input
+int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percentFillWithSandPlantAndPump = double.Parse(Console.ReadLine());
+
+// Calculations
+double aquariumAmountOfWater = length * width * height / 1000.0;
+
+double finalLitters = aquariumAmountOfWater * (1 - percentFillWithSandPlantAndPump / 100);
+
+// Output
+Console.WriteLine($"{finalLitters:F2}");
