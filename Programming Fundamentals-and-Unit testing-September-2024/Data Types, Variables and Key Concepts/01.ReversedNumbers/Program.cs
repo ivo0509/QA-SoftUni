36020da93e10990f4b63da834b8d3f28ac402c05
Ivan Yordanov <55908650+ivo0509@users.noreply.github.com>
2024-10-07 00:12:48 -0900

    Added new exercise by new module in SoftUni
    This is my new exercise from current module Programming Fundamentals and Unit testing (C#)

---
+++ Programming Fundamentals-and-Unit testing-September-2024/Data Types, Variables and Key Concepts/01.ReversedNumbers/Program.cs
@@ -0,0 +1,8 @@
+﻿
+double firstNum = double.Parse(Console.ReadLine());
+double secondNum = double.Parse(Console.ReadLine());
+double thirdNum = double.Parse(Console.ReadLine());
+
+Console.WriteLine(thirdNum);
+Console.WriteLine(secondNum);
+Console.WriteLine(firstNum);
