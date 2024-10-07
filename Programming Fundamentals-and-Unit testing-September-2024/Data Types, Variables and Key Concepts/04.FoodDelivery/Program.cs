f96bcce8a00000c99eddeb1b28b931e11a59b288
Ivan Yordanov <55908650+ivo0509@users.noreply.github.com>
2024-10-07 00:25:43 -0900

    Added more two exercises to new module

---
+++ Programming Fundamentals-and-Unit testing-September-2024/Data Types, Variables and Key Concepts/04.FoodDelivery/Program.cs
@@ -0,0 +1,23 @@
+﻿// Input
+int numberOfChickenMenus = int.Parse(Console.ReadLine());
+int numberOfFishMenus = int.Parse(Console.ReadLine());
+int numberOfVegetarianMenus = int.Parse(Console.ReadLine());
+
+// Calculations
+double priceForOneChickenMenu = 10.35;
+double priceForOneFishMenu = 12.40;
+double priceForOneVegetarianMenu = 8.15;
+double deliveryPrice = 2.50;
+
+double priceForAllChickenMenus = priceForOneChickenMenu * numberOfChickenMenus;
+double priceForaAllFishMenus = priceForOneFishMenu * numberOfFishMenus;
+double priceForAllVegetarianMenus = priceForOneVegetarianMenu * numberOfVegetarianMenus;
+
+double orderPrice = priceForAllChickenMenus + priceForaAllFishMenus + priceForAllVegetarianMenus;
+
+double priceForDessert = orderPrice * 0.2;
+
+double finalPrice = orderPrice + priceForDessert + deliveryPrice;
+
+// Output
+Console.WriteLine(finalPrice);
---
+++ Programming Fundamentals-and-Unit testing-September-2024/Data Types, Variables and Key Concepts/05.BasketballEquipment/Program.cs
@@ -0,0 +1,13 @@
+﻿// Input
+int basketballFeeForOneYEar = int.Parse(Console.ReadLine());
+
+// Calculations
+double priceForSneakers = basketballFeeForOneYEar * 0.6; // -40%
+double priceForUniform = priceForSneakers * 0.8;         // -20%
+double priceForBall = priceForUniform / 4;
+double priceForAccessories = priceForBall / 5;
+
+double finalPrice = basketballFeeForOneYEar + priceForSneakers + priceForUniform + priceForBall + priceForAccessories;
+
+// Output
+Console.WriteLine(finalPrice);
