ce242875c47acdd559802daf62c58c33be1a5327
Ivan Yordanov <55908650+ivo0509@users.noreply.github.com>
2024-10-07 00:23:52 -0900

    Added exercise to new module

---
+++ Programming Fundamentals-and-Unit testing-September-2024/Data Types, Variables and Key Concepts/02.CenturiesToMinutes/Program.cs
@@ -0,0 +1,17 @@
+﻿// Input
+int centuries = int.Parse(Console.ReadLine());
+
+// Calculations
+int years = centuries * 100;
+int days = (int)(years * 365.2422); // 36524.22
+int hours = days * 24;
+int minutes = hours * 60;
+
+// Output
+Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+
+
+
+
+
+
