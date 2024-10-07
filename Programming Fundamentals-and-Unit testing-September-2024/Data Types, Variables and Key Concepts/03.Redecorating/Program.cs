c0cd2cb4c7d679eac928ae644a9e846b3c5a539d
Ivan Yordanov <55908650+ivo0509@users.noreply.github.com>
2024-10-07 00:24:37 -0900

    Added exercise to new module

---
+++ Programming Fundamentals-and-Unit testing-September-2024/Data Types, Variables and Key Concepts/03.Redecorating/Program.cs
@@ -0,0 +1,25 @@
+﻿// Input
+int amountOfNylon = int.Parse(Console.ReadLine());
+int amountOfPaint = int.Parse(Console.ReadLine());
+int amountOfThinner = int.Parse(Console.ReadLine());
+int hoursWork = int.Parse(Console.ReadLine());
+
+// Calculations
+double protectiveNylonPerMeter = 1.50;
+double paintPricePerLitter = 14.50;
+double thinnerPricePerLitter = 5;
+double bagsPrice = 0.4;
+
+double priceForNylon = (amountOfNylon + 2) * protectiveNylonPerMeter;
+double priceForPaint = amountOfPaint * 1.1 * paintPricePerLitter;
+double priceForThinner = amountOfThinner * thinnerPricePerLitter;
+
+double priceForMaterials = priceForNylon + priceForPaint + priceForThinner + bagsPrice;
+
+double priceForOneHourWork = priceForMaterials * 0.3;
+double priceForWorkers = priceForOneHourWork * hoursWork;
+
+double finalPrice = priceForMaterials + priceForWorkers;
+
+// Output
+Console.WriteLine(finalPrice);
